a76115e5f38a2d7be2a45796502dbe59f89b24f9
Myeongjin <aranet100@gmail.com>
2016-01-16 07:25:04 +0100

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['key_types']             = '기본 서식 도구. 유형 나열.  서명 넣기. ';
+$lang['misc']                  = '기타. 피커: 이모티콘 및 특수 문자.  수평자.';
+$lang['key_types_other']       = '사용되지 않음; 체크박스에 선택해야 합니다';
+$lang['misc_other']            = '사용되지 않음; 체크박스에 선택해야 합니다';
+$lang['headers']               = '문단 제목: 하위, 상위 및 동급의 자동 문단 제목 선택기 입니다;  1-5는 문단 제목 선택 메뉴에서의 다섯 수준입니다; 선택은 전체 선택 메뉴입니다.';
+$lang['headers_other']         = '사용되지 않음; 체크박스에 선택해야 합니다';
