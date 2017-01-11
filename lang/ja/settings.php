22fa4dbd5f846210378fa26901f81a8498ab1388
Hideaki SAWADA <chuno@live.jp>
2017-01-11 10:40:04 +0100

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['key_types']             = '基本的な書式設定ツール。一覧の種類。署名の挿入。';
@@ -11,3 +11,4 @@ $lang['key_types_other']       = '未使用。チェックボックスから選�
 $lang['misc_other']            = '未使用。チェックボックスから選択する必要があります。';
 $lang['headers']               = '見出し：低いレベル、高いレベル、同じレベルは自動見出しの選択肢です。１〜５は、見出し選択メニューの５つのレベルです。';
 $lang['headers_other']         = '未使用。チェックボックスから選択する必要があります。';
+$lang['links']                 = 'リンク類：メディア（メディアマネージャー）、内部リンク、外部リンク';
