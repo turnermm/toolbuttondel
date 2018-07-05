72f7c637aff47e7048cde83f2c0c46a19822f1b3
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 14:30:04 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['key_types']             = 'Ferramentas básicas de formatação. Tipos de listas. Inserir assinatura.';
+$lang['misc']                  = 'Diversos. Seletores: Smileys e Personagens Especiais. Regra horizontal.';
+$lang['key_types_other']       = 'Não utilizado; você deve selecionar as caixas de seleção';
+$lang['misc_other']            = 'Não utilizado; você deve selecionar as caixas de seleção';
+$lang['headers']               = 'Títulos: mais baixos, mais altos e iguais são os seletores de cabeçalho automático; 1-5 são os cinco níveis no menu de seleção de títulos; select é o menu de seleção inteiro.';
+$lang['headers_other']         = 'Não utilizado; você deve selecionar as caixas de seleção';
+$lang['links']                 = 'Tipos de links: media (mediamanager), links internos (linkwiz), links externos';
